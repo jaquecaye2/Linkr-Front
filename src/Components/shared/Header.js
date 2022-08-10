f6eb1dbc826169494191480f5346f5779964ced8
jaquecaye <jaquelinecaye@gmail.com>
2022-08-09 22:14:34 -0300

    Implementação layout estatico Tela Timeline

--- src/Components/shared/Header.js
+++ src/Components/shared/Header.js
@@ -1,7 +1,41 @@
-export default function Header(){
-    return(
-        <div>
-            <h1>Este é o Header</h1>
-        </div>
-    )
+import styled from "styled-components";
+
+import perfil from "../../assets/images/perfil.jpeg"
+
+export default function Header() {
+  return (
+    <HeaderStyle>
+      <h1>linkr</h1>
+      <img
+        src={perfil}
+        alt="Foto de perfil"
+      />
+    </HeaderStyle>
+  );
 }
+
+const HeaderStyle = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 72px;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  background-color: var(--cor-header);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+
+  h1 {
+    font-size: 49px;
+    font-weight: 700;
+  }
+
+  img {
+    width: 53px;
+    height: 53px;
+    object-fit: cover;
+    border-radius: 60px;
+  }
+`;
