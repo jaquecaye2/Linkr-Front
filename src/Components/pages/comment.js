7f9bf8518e558d334aca93b3c3e9c41b053e2d64
MariaDuda <mariabevenuto062@gmail.com>
2022-08-16 15:19:51 -0300

    mensagem feita

--- src/Components/pages/comment.js
+++ src/Components/pages/comment.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
+import testeImg from "../../assets/images/testeImg.png"
 
 
 export default function Chat() {
     return (
         <ChatConteudo>
-            <h1>oi</h1>
+            <div>
+                <img src={testeImg} alt="Foto" />
+            </div>
+            <Comment>
+                <Status>
+                    <h3>João Avatares</h3>
+                    <p>• following</p>
+                </Status>
+                <Comentario>
+                Adorei esse post, ajuda muito a usar Material UI com React!
+                </Comentario>
+            </Comment>
         </ChatConteudo>
     )
 }
@@ -12,13 +24,63 @@ export default function Chat() {
 const ChatConteudo = styled.div`
   width: 100%;
   display: flex;
-  background-color: pink;
-  border-radius: 15px;
+  background-color: #1E1E1E;
+  border-radius: 16px;
   padding: 20px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   margin-bottom: 16px;
   @media (max-width: 614px) {
     border-radius: 0;
   }
+  img {
+      width: 39px;
+      height: 39px;
+      object-fit: cover;
+      border-radius: 60px;
+      margin-bottom: 15px;
+      :hover {
+        cursor: pointer;
+      }
+    }
+`
+const Comment = styled.div`
+margin-left: 3%;
+
+`
+
+const Status = styled.div`
+width: 100%;
+margin-left: 3%;
+display: flex;
+h3{
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 17px;
+    color: #F3F3F3
+}
+p{
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    color: #565656;
+    margin-left: 10px;
+}
+
+`
+
+const Comentario = styled.div`
+width: 100%;
+margin-left: 3%;
+font-family: 'Lato';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 17px;
+color: #ACACAC;
+margin-top: 1%;
 
 `
